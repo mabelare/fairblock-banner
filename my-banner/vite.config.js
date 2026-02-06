ddad502a1d59312e663e7fcdebc317291a7d547a
mabel <mabelare293@gmail.com>
2026-02-06 20:58:30 +0100

    Add Netlify configuration

--- my-banner/vite.config.js
+++ my-banner/vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
